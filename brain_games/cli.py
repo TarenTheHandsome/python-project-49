bc4c16e44a74610cd4ef5793621667524327befb
Elena <ziggy.taren.44@gmail.com>
2024-02-28 01:29:26 +0300

    Third step of a project :D

---
+++ brain_games/cli.py
@@ -0,0 +1,13 @@
+import prompt
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello,{name}!')
+    return name
+
+def main():
+    welcome_user()
+
+
+if __name__ == '__main__':
+    main()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+from ..cli import welcome_user
+
 def print_welcome():
     print('Welcome to the Brain Games!')
+    welcome_user()
+
 
 def main():
     print_welcome()
